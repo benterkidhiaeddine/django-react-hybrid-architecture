4d6020675059b84720923972d2f42e2d206040ae
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-12-25 15:03:50 +0100

    Add validation using bootstrap forms

--- crm/front-end/Components/EmployeeDetail.js
+++ crm/front-end/Components/EmployeeDetail.js
@@ -12,6 +12,9 @@ export function EmployeeDetail({ employeeId }) {
 
   const employeesURL = `api/employees/${employeeId}/`;
 
+  //Validation state variables
+  const [validated, setValidated] = useState(false);
+
   useEffect(() => {
     fetch(employeesURL)
       .then((response) => response.json())
@@ -26,6 +29,14 @@ export function EmployeeDetail({ employeeId }) {
   //Function that handles the submit of the form
   function handleSubmit(e) {
     e.preventDefault();
+
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation;
+    }
+
+    setValidated(true);
+
     const employee = { name, department, salary };
     fetch(employeesURL, {
       method: "PUT",
@@ -40,21 +51,27 @@ export function EmployeeDetail({ employeeId }) {
   }
 
   return (
-    <Form>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="name">
         <Form.Label>Name</Form.Label>
         <Form.Control
+          required
           onChange={(e) => setName(e.target.value)}
           type="text"
           placeholder="Enter the name"
           value={name}
         />
         <Form.Text className="text-muted">The name of your employee</Form.Text>
+        <Form.Control.Feedback type="invalid">
+          Pleas input a name
+        </Form.Control.Feedback>
+        <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Department</Form.Label>
         <Form.Select
+          required
           onChange={(e) => setDepartment(e.target.value)}
           aria-label="Default select example"
           value={department}
@@ -70,11 +87,17 @@ export function EmployeeDetail({ employeeId }) {
         <Form.Text className="text-muted">
           The department that your employee is part of
         </Form.Text>
+
+        <Form.Control.Feedback type="invalid">
+          Pleas select a department
+        </Form.Control.Feedback>
+        <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="salary">
         <Form.Label>Salary</Form.Label>
         <Form.Control
+          required
           onChange={(e) => setSalary(e.target.value)}
           type="number"
           value={salary}
@@ -82,9 +105,13 @@ export function EmployeeDetail({ employeeId }) {
         <Form.Text className="text-muted">
           Enter the salary of your employee
         </Form.Text>
+        <Form.Control.Feedback type="invalid">
+          Pleas enter a valid salary
+        </Form.Control.Feedback>
+        <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
       </Form.Group>
 
-      <Button onClick={handleSubmit} variant="primary" type="submit">
+      <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
